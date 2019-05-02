c4fc6a3d987f000210b9f52ef90d814ca1a3b5a2
Andrew Brethauer <39843881+aBrethauer@users.noreply.github.com>
2019-05-02 10:20:00 -0500

    Updates: Add express server Install body parser to dependencies Add Model folder with initial model .js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-//Setup Variables
-
+//Setup Dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+//Setup variables
+const PORT = process.env.PORT  || 3000 //Establishes port to listen on
+var app = express();
+
+
+
+
+
+
+//Setup express web server
+app.listen(PORT, () => {
+    console.log(`Server is working on port ${PORT}`); //Confirms server is working with console message
+})
